9fd794cdbf13f1a8eaef6724dbd765f6d83745f5
renardbergson <renard.contato@gmail.com>
2025-07-24 00:14:47 -0300

    chore: add eslint sort plugin and prettier tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import { Input } from "@/components/ui/input";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-red-500">Welcome to FSW Donalds</h1>
+      <h1 className="m-5 p-10 text-red-500">Welcome to FSW Donalds</h1>
       <p>Your favorite fast food restaurant</p>
       <Input placeholder="Search for food..." className="mb-4" />
       <Button variant={"default"}>Order Now</Button>
     </div>
   );
-}
- 
+};
+
 export default HomePage;
